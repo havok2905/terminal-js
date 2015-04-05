6cc7ce96cf69aec3c57cc00689426b3ffe6cfe63
Christopher McLean <havok2905@gmail.com>
2015-04-05 16:20:23 -0400

    parsing and command class

---
+++ app/terminal/command.js
@@ -0,0 +1,95 @@
+var Command;
+
+Command = function(command, flags, arguments, after) {
+  this.command = command;
+  this.flags = flags;
+  this.arguments = arguments;
+  this.after = after;
+};
+
+Command.commands = [
+  'ls',
+  'cd',
+  'grep',
+  'cat',
+  'pwd'
+];
+
+Command.exists = function(command) {
+  return Command.commands.indexOf(command) !== -1 ? true : false;
+};
+
+
+/*
+ * locate files with the given keyword
+ */
+
+Command.grep = function(working) {
+
+}
+
+
+/*
+ * Navigate through the file system
+ * Only supports single directory navigation currently
+ */
+
+Command.cd = function(working, target) {
+  if(target === '/' || target === '/~') {
+    return working.root;
+  }
+  else if(target === '.' || target === '') {
+    return working;
+  }
+  else if(target === '..') {
+    if(working.parent !== null) {
+      return working.parent;
+    }
+  }
+  else {
+    return working.contents.filter(function(item) {
+      return item instanceof Directory && item.name === target;
+    })[0];
+  }
+};
+
+
+/*
+ * List current contents at your location in the filesystem
+ */
+
+Command.ls = function(working) {
+  return working.contents.map(function(item) {
+    return item.fullname();
+  });
+};
+
+
+/*
+ * Return your current location in the file system.
+ */
+
+Command.pwd = function(working) {
+  function recurse(current, path) {
+    if(current.parent === null) {
+      path.push('~');
+      return path;
+    } else {
+      path.push(current.name);
+      return recurse(current.parent, path);
+    }
+  }
+
+  return recurse(working, []).reverse();
+};
+
+
+/*
+ * Return the contents of a file at your current location
+ */
+
+Command.cat = function(working, target) {
+  return working.contents.filter(function(item) {
+    return item instanceof File && (item.name + '.' + item.extension) === target;
+  })[0].text;
+};
--- app/terminal/system.js
+++ app/terminal/system.js
@@ -1,53 +1,10 @@
-var System;
+var System,
+    commands,
+    command;
 
 System = function(contents) {
   this.tree = new Directory('~', null, null, contents); // Master cached copy of file system
   this.working = new Directory('~', null, null, contents); // Working directory for reference
-};
-
-
-/*
- * Return a list of system commands that a user can use at face value
- */
-
-System.prototype.help = function() {
-  return ['ls', 'cd', 'pwd', 'cat'];
-};
-
-
-/*
- * Navigate through the file system
- * Only supports single directory navigation currently
- */
-
-System.prototype.cd = function(target) {
-  if(target === '/' || target === '/~') {
-    this.working = this.working.root;
-  }
-  else if(target === '.' || target === '') {
-    // DO NOTHING
-  }
-  else if(target === '..') {
-    if(this.working.parent !== null) {
-      this.working = this.working.parent;
-    }
-  }
-  else {
-    this.working = this.working.contents.filter(function(item) {
-      return item instanceof Directory && item.name === target;
-    })[0];
-  }
-};
-
-
-/*
- * List current contents at your location in the filesystem
- */
-
-System.prototype.ls = function() {
-  return this.working.contents.map(function(item) {
-    return item.fullname();
-  });
 };
 
 
@@ -62,52 +19,26 @@ System.prototype.traverse = function(target) {
 
 
 /*
- * Return your current location in the file system.
- */
-
-System.prototype.pwd = function() {
-  function recurse(current, path) {
-    if(current.parent === null) {
-      path.push('~');
-      return path;
-    } else {
-      path.push(current.name);
-      return recurse(current.parent, path);
-    }
-  }
-
-  return recurse(this.working, []).reverse();
-};
-
-
-/*
- * Return the contents of a file at your current location
- */
-
-System.prototype.cat = function(target) {
-  return this.working.contents.filter(function(item) {
-    return item instanceof File && (item.name + '.' + item.extension) === target;
-  })[0].text;
-};
-
-
-/*
  * Interface for user commands
  */
 
 System.prototype.exec = function(command, argument) {
   switch(command) {
     case 'ls':
-      return this.ls();
+      return Command.ls(this.working);
       break;
     case 'pwd':
-      return this.pwd();
+      return Command.pwd(this.working);
       break;
     case 'cd':
-      return this.cd(argument);
+      this.working = Command.cd(this.working, argument);
+      return this.working;
       break;
     case 'cat':
-      return this.cat(argument);
+      return Command.cat(this.working, argument);
+      break;
+    case 'grep':
+      return Command.grep(this.working);
       break;
     default:
       return false;
@@ -116,13 +47,36 @@ System.prototype.exec = function(command, argument) {
 
 
 /*
- * Parse user input into commands
+ * Split the string of commands into an array of components.
+ * Generate new Command objects from the list and return them.
  */
 
 System.prototype.parse = function(input) {
-  commands = input.split(/\| | &&/).map(function(item) {
-    return item.trim().split(' ');
+  commands = [];
+  command = new Command(null, [], [], null);
+
+  input.split(' ').forEach(function(item) {
+    if(item.match(/^-/)) {
+      item = item.substr(1).split('');
+      item.forEach(function(flag) {
+        command.flags.push(flag);
+      })
+    }
+    else if(Command.exists(item)) {
+      command.command = item;
+    }
+    else if(item !== '&&' && item !== '|') {
+      command.arguments.push(item);
+    }
+    else {
+      command.after = item;
+      commands.push(command);
+      command = new Command(null, [], [], null);
+    }
   });
+
+  commands.push(command);
+  command = new Command(null, [], [], null);
 
   return commands;
 };
